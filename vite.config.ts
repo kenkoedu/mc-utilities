8e487e5320d6ad6d666c82af7054389754a85e1e
Ken Ko <personal@kenko.science>
2024-02-06 03:03:06 +0800

    include assets

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { BootstrapVueNextResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/mc-utilities/",
+  assetsInclude: ['**/*.png'],
   plugins: [vue(),
   Components({
     resolvers: [BootstrapVueNextResolver()],
